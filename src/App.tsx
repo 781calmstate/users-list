84d051b868f82ce3800d78ff6b9c79ba59bbf504
781calmstate <frontenddevjscript@gmail.com>
2023-06-03 18:06:00 +0300

    added content to UsersPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import './components/styles/App.css';
 import WelcomePage from './components/pages/WelcomePage/WelcomePage';
 import UsersPage from './components/pages/UsersPage/UsersPage';
 import NotFoundPage from './components/pages/NotFoundPage/NotFoundPage';
