5f9a325823bd07d66d667e5e03abdd5457489b3d
781calmstate <frontenddevjscript@gmail.com>
2023-06-11 20:34:40 +0300

    added content to 404 page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import '../../styles/NotFoundPage.css';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <section className="page_404">
+      <div className="container">
+        <div className="row">
+          <div className="four_zero_four_bg">
+            <h1 className="text-center ">404</h1>
+          </div>
+
+          <div className="content_box_404">
+            <h3 className="text-center">{`Look like you're lost`}</h3>
+
+            <p>the page you are looking for not available!</p>
+
+            <Link to="/users-list/users" className="link_404">
+              Go to Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundPage;
