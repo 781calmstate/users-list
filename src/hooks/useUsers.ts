8015f3435c2c8c16eeef655f0968fb5fb84a3e95
DMV <DMV@d.c>
2023-06-29 17:39:33 +0300

    optimized functions in useUsers

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -3,14 +3,18 @@ import { IUser } from '../types/model';
 
 export const useSortedUsers = (users: IUser[], sort: string) => {
   const sortedUsers = useMemo(() => {
-    if (sort && (sort === 'name' || sort === 'username')) {
-      return [...users].sort((a, b) => a[sort].localeCompare(b[sort]));
-    } else if (sort === 'id') {
-      return [...users].sort((a, b) => Number(b.id) - Number(a.id));
-    } else {
-      return users;
-    }
+    const compareFn = (a: IUser, b: IUser) => {
+      if (sort === 'name' || sort === 'username') {
+        return a[sort].localeCompare(b[sort]);
+      } else if (sort === 'id') {
+        return Number(b.id) - Number(a.id);
+      }
+      return 0;
+    };
+
+    return sort ? [...users].sort(compareFn) : users;
   }, [sort, users]);
+
   return sortedUsers;
 };
 
@@ -18,11 +22,13 @@ export const useUsers = (users: IUser[], query: string, sort: string) => {
   const sortedUsers = useSortedUsers(users, sort);
 
   const sortedAndSearched = useMemo(() => {
+    const lowercasedQuery = query.toLocaleLowerCase();
+
     return sortedUsers.filter(
       (user) =>
-        user.name.toLowerCase().includes(query.toLowerCase()) ||
-        user.username.toLowerCase().includes(query.toLowerCase())
+        user.name.toLowerCase().includes(lowercasedQuery) ||
+        user.username.toLowerCase().includes(lowercasedQuery)
     );
-  }, [query, sortedUsers, sort]);
+  }, [query, sortedUsers]);
   return sortedAndSearched;
 };
